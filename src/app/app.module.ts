bc3cb91dc0ed24f5bc20b7d718eed6a7e76ccc4d
Iain Jenkins <63315736+Inferno-Dragon@users.noreply.github.com>
2020-11-05 06:59:07 +0200

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CvComponent } from './components/cv/cv.component';
+import { ContactMeComponent } from './components/contact-me/contact-me.component';
+import { ContactMeFormComponent } from './components/contact-me-form/contact-me-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CvComponent,
+    ContactMeComponent,
+    ContactMeFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/contact-me/contact-me.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ContactMeFormComponent } from '../contact-me-form/contact-me-form.component';
+
+@Component({
+  selector: 'app-contact-me',
+  templateUrl: './contact-me.component.html',
+  styleUrls: ['./contact-me.component.css']
+})
+export class ContactMeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
